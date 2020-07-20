154363d8bc2df92841d0c592892afd74348f958a
MarceloHeredia <heredia_m@outlook.com>
2020-07-20 02:51:44 -0300

    version 1.0

---
+++ VendedorInterespacial.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace VendedorInterespacial
+{
+    partial class VendedorInterespacial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VendedorInterespacial));
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.AllowDrop = true;
+            this.textInput.Location = new System.Drawing.Point(12, 41);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textInput.Size = new System.Drawing.Size(300, 137);
+            this.textInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Entrada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Saida";
+            // 
+            // textOutput
+            // 
+            this.textOutput.AllowDrop = true;
+            this.textOutput.Location = new System.Drawing.Point(12, 213);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ReadOnly = true;
+            this.textOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textOutput.Size = new System.Drawing.Size(300, 137);
+            this.textOutput.TabIndex = 2;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(327, 183);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 27);
+            this.btnExecutar.TabIndex = 4;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // VendedorInterespacial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(591, 394);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(607, 433);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(607, 433);
+            this.Name = "VendedorInterespacial";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "VendedorInterespacial";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ VendedorInterespacial.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VendedorInterespacial
+{
+    public partial class VendedorInterespacial : Form
+    {
+        public VendedorInterespacial()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Dictionary<String, Int32> _nmbrs = new Dictionary<string, int>()
+        {
+            {"I", 1 },{"V", 5 },{"X",10},{"L",50 },{"C",100 },{"D",500 },{"M",1000}
+        };
+
+        Dictionary<String, String> _represents = new Dictionary<string, string>();
+        Dictionary<String, Double> _mineralValue = new Dictionary<string, double>(); //unit value of mineral
+        String newLine = Environment.NewLine;
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            this.textOutput.Text = ""; //cleans text Output
+            _represents = new Dictionary<string, string>();
+            _mineralValue = new Dictionary<string, double>(); //unit value of mineral
+
+            var entry_lines = this.textInput.Text.Split('\n'); //splits lines in entries
+
+            foreach (var entry in entry_lines)
+            {
+                var splitEntry = entry.Split(' ');
+                if (splitEntry.Contains("representa"))
+                {
+                    InsertRepresentation(splitEntry[0], splitEntry[2]);
+                }
+                else if (splitEntry.Contains("valem"))
+                {
+                    int index = Array.IndexOf<String>(splitEntry, "valem");
+                    String[] valuesnMineral = new string[index];
+                    for (int i = 0; i < index; i++)
+                    {
+                        valuesnMineral[i] = splitEntry[i];
+                    }
+                    Int32 credits = Convert.ToInt32(splitEntry[index + 1]);
+
+                    CalculateQuantity(valuesnMineral, credits);
+                }
+                else if (splitEntry.Contains("quanto"))
+                {
+                    //quanto vale .... ?
+                    QuantoVale(splitEntry);
+                }
+                else
+                { //quantos creditos sao
+                    QuantosCreditosSao(splitEntry);
+                }
+            }
+
+        }
+
+        private void InsertRepresentation(string name, string equivalent)
+        {
+            if (_represents.Count < 7)
+            {
+                if (equivalent.Contains("\r") || equivalent.Contains("\n"))
+                {
+                    equivalent = equivalent.Substring(0, 1);
+                }
+                equivalent = equivalent.ToUpper();
+                _represents.Add(name, equivalent);
+            }
+        }
+
+        //calculates the value of a single mineral from the mineral type and saves it to the dictionary mineralValue
+        private void CalculateQuantity(String[] valuesAndMineral, Int32 numberCredits)
+        {
+            String mineral = valuesAndMineral.Last();
+
+            Int32 value = 0;
+            //ignores the last element which is the mineral name
+            for (int i=0; i<valuesAndMineral.Count()-1; i++)
+            {                                    //number value of [ representation value of [ alien notation] ] 
+                var integerNotation = _nmbrs[_represents[valuesAndMineral[i]]];
+                if (i < valuesAndMineral.Count() - 2)
+                {                                                   //next number
+                    if (integerNotation < _nmbrs[_represents[valuesAndMineral[i + 1]]])
+                    {
+                        integerNotation = -integerNotation; //if its smaller than the next number, its a subtraction
+                    }
+                }
+                value += integerNotation;
+            }
+            double finalValue = Convert.ToDouble(numberCredits) / Convert.ToDouble(value);
+            try
+            {
+                _mineralValue.Add(mineral, finalValue);
+            }
+            catch (Exception ex)
+            {
+                this.textOutput.AppendText(ex.Message + newLine);
+            }
+        }
+
+        private void QuantoVale(string[] qtVale)
+        {
+            try
+            {
+                Int32 value = 0;
+                for (int i = 2; i < qtVale.Count() - 1; i++)
+                {
+                    //number value of [ representation value of [ alien notation] ] 
+                    var integerNotation = _nmbrs[_represents[qtVale[i]]];
+                    if (i < qtVale.Count() - 2)
+                    {                                                   //next number
+                        if (integerNotation < _nmbrs[_represents[qtVale[i + 1]]])
+                        {
+                            integerNotation = -integerNotation; //if its smaller than the next number, its a subtraction
+                        }
+                    }
+                    value += integerNotation;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int i =2; i<qtVale.Count()-1; i++)
+                {
+                    sb.Append(qtVale[i] + " ");
+                }
+                sb.Append("vale "+ Convert.ToString(value));
+
+                this.textOutput.AppendText(sb.ToString() + newLine);
+            }
+            catch (Exception)
+            {
+                this.textOutput.AppendText("Nem ideia do que isto significa!"+newLine);
+            }
+        }
+
+        private void QuantosCreditosSao(string[] qtSao)
+        {
+            try
+            {
+                //how many minerals
+                Int32 qts = 0;
+                for (int i = 3; i < qtSao.Count() - 2; i++)
+                {
+                    //number value of [ representation value of [ alien notation] ] 
+                    var integerNotation = _nmbrs[_represents[qtSao[i]]];
+                    if (i < qtSao.Count() - 3)
+                    {                                                   //next number
+                        if (integerNotation < _nmbrs[_represents[qtSao[i + 1]]])
+                        {
+                            integerNotation = -integerNotation; //if its smaller than the next number, its a subtraction
+                        }
+                    }
+                    qts += integerNotation;
+                }
+                String mineral = qtSao[qtSao.Count()-2];
+                Double value = _mineralValue[mineral] * qts;
+
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 3; i < qtSao.Count() - 1; i++)
+                {
+                    sb.Append(qtSao[i] + " ");
+                }
+                sb.Append("custa " + Convert.ToString(value) + " créditos");
+
+                this.textOutput.AppendText(sb.ToString() + newLine);
+            }
+            catch (Exception)
+            {
+                this.textOutput.AppendText("Nem ideia do que isto significa!"+ newLine);
+            }
+        }
+
+    }
+}
